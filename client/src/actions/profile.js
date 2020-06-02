0b2a9414132990a43e289df46ad1ef68ef50965b
horlorlahdeh <horlorlahdeh@gmail.com>
2020-06-02 09:12:04 +0100

    Create Profile Actions

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -34,7 +34,7 @@ export const createProfile = (formData, history, edit = false) => async (
       type: GET_PROFILE,
       payload: res.data,
     });
-    dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created'));
+    dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success'));
 
     if (!edit) {
       history.push('/dashboard');
