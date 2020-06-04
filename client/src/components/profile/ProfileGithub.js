917b31c369ba36d8dd96556512ffc3d8cd618414
horlorlahdeh <horlorlahdeh@gmail.com>
2020-06-04 04:48:54 +0100

    Repo displayed

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -26,6 +26,18 @@ const ProfileGithub = ({ username, getGithubRepos, repos }) => {
                   {repo.name}
                 </a>
               </h4>
+              <p>{repo.description}</p>
+            </div>
+            <div>
+              <ul>
+                <li className='badge badge-primary'>
+                  Stars: {repo.statgazers_count}
+                </li>
+                <li className='badge badge-dark'>
+                  Watchers: {repo.watchers_count}
+                </li>
+                <li className='badge badge-light'>Forks: {repo.forks_count}</li>
+              </ul>
             </div>
           </div>
         ))
