135046e6bc8263d3f4b197ee27b708bc18a250bc
horlorlahdeh <horlorlahdeh@gmail.com>
2020-06-05 03:44:41 +0100

    deploy to heroku

--- Server.js
+++ Server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 connectDB();
 
 // Init Middleware
-app.use(express.json({extended: false}));
+app.use(express.json({ extended: false }));
 
 const usersRoute = require('./routes/api/users');
 const profileRoute = require('./routes/api/profile');
@@ -17,6 +18,16 @@ app.use('/api/users', usersRoute);
 app.use('/api/profile', profileRoute);
 app.use('/api/posts', postsRoute);
 app.use('/api/auth', authRoute);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
